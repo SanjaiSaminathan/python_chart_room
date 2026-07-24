280cf7abc7ad48ead0e7a155e37df4550fecf9ef
Sanja <sanjaisaminathan5032@gmail.com>
2026-07-24 10:02:23 +0530

    create index html in template folder . replace the user interface javascript to html.i update the app.py also thank you

--- app.py
+++ app.py
@@ -2,16 +2,23 @@
 
 from fastapi import FastAPI
 
+from fastapi.responses import HTMLResponse
+
+from fastapi.templating import Jinja2Templates
+
+from fastapi import Request
 # create an instance object of the fastAPI class
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory="templates")
 # decorator
 
-@app.get("/")
-
-def home():
-    return {"message": "come back to the chat room"}
+@app.get("/", response_class=HTMLResponse)
 
 
-
+def home(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="index.html"
+    )
