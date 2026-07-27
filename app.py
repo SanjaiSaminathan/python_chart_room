e8772f0f7bfa297a648a7ac911c0ba47e1568c3f
Sanja <sanjaisaminathan5032@gmail.com>
2026-07-27 08:37:14 +0530

    Use connection manager for WebSocket

--- app.py
+++ app.py
@@ -1,38 +1,27 @@
-# import the fastapi library
-
 from fastapi import FastAPI
-
 from fastapi.responses import HTMLResponse
-
 from fastapi.templating import Jinja2Templates
-
 from fastapi import Request
-
-
 from fastapi import Form
-
-# import the websocket library
-
 from fastapi import WebSocket
 
-# create an instance object of the fastAPI class
+from manager import ConnectionManager
 
 app = FastAPI()
 
+manager = ConnectionManager()
+
 templates = Jinja2Templates(directory="templates")
-# decorator
+
 
 @app.get("/", response_class=HTMLResponse)
-
-
 def home(request: Request):
     return templates.TemplateResponse(
         request=request,
         name="index.html"
     )
-    
-    
-@app.post("/chat")
+
+
 @app.post("/chat", response_class=HTMLResponse)
 def join_chat(
     request: Request,
@@ -47,15 +36,19 @@ def join_chat(
             "room": room
         }
     )
-        
-# websocket endpoint for chat room
+
+
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
 
-    while True:
-        message = await websocket.receive_text()
+    await manager.connect(websocket)
 
-        await websocket.send_text(
-            f"You typed: {message}"
-        )
+    try:
+        while True:
+
+            message = await websocket.receive_text()
+
+            await manager.broadcast(message)
+
+    except:
+        manager.disconnect(websocket)
